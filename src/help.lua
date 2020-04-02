9d9683011f0492ec33d2eb9159d34f34a57112eb
Blake Mitchell <mblakeandrew@gmail.com>
2020-04-01 23:03:00 -0700

    added empty mouse event functions to stop program from crashing when mouse is clicked while shortcut menu is open

--- src/help.lua
+++ src/help.lua
@@ -1,3 +1,13 @@
 Help = {}
 
 Help.name = "shortcuts  \n B: freehand draw (Brush)  \n L: Line tool  \n E: Eraser  \n P: Pan/zoom  \n G: Grab  \n M: Move  \n S: Smooth  \n F: Flatten  \n R: Rectangular selection  \n N: eNvelope tool  \n D: Deselect  \n [ and ]: chage brush radius (when applicable)  \n Space: play/pause  \n ctrl+Z: undo  \n ctrl+Y / ctrl+shift+Z: redo  \n ctrl+S: save  \n ctrl+O: open save folder"
+
+function Help.mousepressed()
+end
+
+function Help.mousedown()
+end
+
+function Help.mousereleased()
+end
+
