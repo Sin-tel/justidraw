b30d8172a1f491c5c46ae6a80116ca3f436331fd
Sintel <sintel.inquiries@gmail.com>
2023-09-19 13:40:52 +0200

    multiline string help

--- src/tool_help.lua
+++ src/tool_help.lua
@@ -1,8 +1,26 @@
 Help = {}
 
-Help.name =
-	"shortcuts  \n B: freehand draw (Brush)  \n L: Line tool  \n E: Eraser  \n P: Pan/zoom  \n G: Grab  \n M: Move  \n S: Smooth  \n F: Flatten  \n R: Rectangular selection  \n N: eNvelope tool  \n D: Deselect  \n [ and ]: chage brush radius (when applicable)  \n Space: play/pause  \n ctrl+Z: undo  \n ctrl+Y / ctrl+shift+Z: redo  \n ctrl+S: save  \n ctrl+O: open save folder"
-
+Help.name = [[
+shortcuts
+ B: freehand draw (Brush)
+ L: Line tool
+ E: Eraser
+ P: Pan/zoom
+ G: Grab
+ M: Move
+ S: Smooth
+ F: Flatten
+ R: Rectangular selection  
+ N: eNvelope tool
+ D: Deselect
+ Y: Show harmonics
+ [ and ]: chage brush radius (when applicable)
+ Space: play/pause
+ ctrl+Z: undo
+ ctrl+Y / ctrl+shift+Z: redo
+ ctrl+S: save
+ ctrl+O: open save folder
+]]
 function Help.mousepressed() end
 
 function Help.mousedown() end
